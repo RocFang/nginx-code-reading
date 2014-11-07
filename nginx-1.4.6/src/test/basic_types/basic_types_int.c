adaea0edd4f45e2646732279b405067aef8817f0
fangpeng <fangpengwansui@126.com>
2014-11-07 17:17:32 +0800

    first excutable test file

--- nginx-1.4.6/src/test/basic_types/basic_types_int.c
+++ nginx-1.4.6/src/test/basic_types/basic_types_int.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-#include "../../core/ngx_config.h"
 
 int main()
 {
-    ngx_uint_t a;
-    ngx_int_t b;
-    a = 1000;
-    b = -1000;
-    printf ("%d + %d = %d\n", a, b, a+b);
+    printf("hello, world!\n");
     return 0;
 }
