948d0cba7ede24d972c836aaf80964eb5702d210
fangpeng <fangpengwansui@126.com>
2014-11-07 12:42:43 +0800

    init

---
+++ nginx-1.4.6/src/test/basic_types/basic_types_int.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "../../core/ngx_config.h"
+
+int main()
+{
+    ngx_uint_t a;
+    ngx_int_t b;
+    a = 1000;
+    b = -1000;
+    printf ("%d + %d = %d\n", a, b, a+b);
+    return 0;
+}
