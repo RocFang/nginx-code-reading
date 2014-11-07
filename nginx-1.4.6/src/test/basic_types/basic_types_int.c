cca63c89816ca2ef96a5d0247c782f2d03ead12e
fangpeng <fangpengwansui@126.com>
2014-11-07 17:23:14 +0800

    test for ngx_int_t

--- nginx-1.4.6/src/test/basic_types/basic_types_int.c
+++ nginx-1.4.6/src/test/basic_types/basic_types_int.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include "../../core/ngx_config.h"
 
 int main()
 {
-    printf("hello, world!\n");
+    ngx_uint_t a;
+    ngx_int_t b;
+    a = 1000;
+    b = -1000;
+    printf ("%d + %d = %d\n", a, b, a+b);
     return 0;
 }
