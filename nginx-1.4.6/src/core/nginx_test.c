90f6559e2e7b714bb50d273ec77ece8a48409391
fangpeng <fangpengwansui@126.com>
2014-11-17 12:50:04 +0800

    add testcase for ngx_queue

--- nginx-1.4.6/src/core/nginx_test.c
+++ nginx-1.4.6/src/core/nginx_test.c
@@ -220,12 +220,19 @@ main(int argc, char *const *argv)
     test_ngx_palloc();                                                                                  
     printf("\n\n");
 
-
     /* test case for ngx_palloc interface*/
     printf("test case for ngx_array interface:\n");
     printf("==================================\n");
     test_ngx_array();
     printf("\n\n");
+ 
+    /* test case for ngx_queue interface*/                                                            
+    printf("test case for ngx_queue interface:\n");                                                    
+    printf("==================================\n");                                                    
+    test_ngx_queue();                                                                                  
+    printf("\n\n"); 
+
+
 
     return 0;
 }
@@ -322,6 +329,70 @@ test_ngx_array()
    
 }  
 
+static ngx_int_t yahoo_no_cmp(const ngx_queue_t* p, const ngx_queue_t* n)
+{
+    yahoo_guy_t *pre, *next;
+    pre  = (yahoo_guy_t*) ngx_queue_data(p, yahoo_guy_t, queue);
+    next = (yahoo_guy_t*) ngx_queue_data(n, yahoo_guy_t, queue);
+    return ((pre->id > next->id) ? 1:0);
+}
+
+static ngx_int_t
+test_ngx_queue()
+{
+    ngx_pool_t*     pool;
+    yahoo_guy_t*    guy;
+    ngx_queue_t*    q;
+    yahoo_t*        yahoo;
+    pool            = ngx_create_pool(1024*10, NULL); //初始化内存池
+    int             i;
+    // 构建队列
+    const ngx_str_t   names[] = {
+        ngx_string("rainx"), ngx_string("xiaozhe"), ngx_string("zhoujian")
+    } ;
+    const int       ids[]   = {4611, 8322, 6111};
+
+    yahoo = ngx_palloc(pool, sizeof(yahoo_t));
+    ngx_queue_init(&yahoo->queue); //初始化queue
+
+    for(i = 0; i < 3; i++)
+    {
+      guy = (yahoo_guy_t*) ngx_palloc(pool, sizeof(yahoo_guy_t));
+      guy->id   = ids[i];
+      //guy->name = (char*) ngx_palloc(pool, (size_t) (strlen(names[i]) + 1) );
+      guy->name = (u_char*) ngx_pstrdup(pool, (ngx_str_t*) &(names[i]) );
+
+      ngx_queue_init(&guy->queue);
+      // 从头部进入队列
+      ngx_queue_insert_head(&yahoo->queue, &guy->queue);
+    }
+
+    // 从尾部遍历输出
+    for(q = ngx_queue_last(&yahoo->queue);
+        q != ngx_queue_sentinel(&yahoo->queue);
+        q = ngx_queue_prev(q) ) {
+
+        guy = ngx_queue_data(q, yahoo_guy_t, queue);
+        printf("No. %d guy in yahoo is %s \n", guy->id, guy->name);
+    }
+
+    // 排序从头部输出
+    ngx_queue_sort(&yahoo->queue, yahoo_no_cmp);
+    printf("sorting....\n");
+    for(q = ngx_queue_prev(&yahoo->queue);
+        q != ngx_queue_sentinel(&yahoo->queue);
+        q = ngx_queue_last(q) ) {
+
+        guy = ngx_queue_data(q, yahoo_guy_t, queue);
+        printf("No. %d guy in yahoo is %s \n", guy->id, guy->name);
+    }
+
+    ngx_destroy_pool(pool);
+    return 0;    
+}
+
+/******************************************************************
+******************************************************************/
 static ngx_int_t
 ngx_add_inherited_sockets(ngx_cycle_t *cycle)
 {
--- nginx-1.4.6/src/core/nginx_test.h
+++ nginx-1.4.6/src/core/nginx_test.h
@@ -4,3 +4,16 @@ static ngx_int_t test_ngx_palloc();
 static void test_ngx_pool_cleanup();
 static void test_ngx_pool_cleanup_2();
 static ngx_int_t test_ngx_array();  
+static ngx_int_t test_ngx_queue();  
+static ngx_int_t yahoo_no_cmp(const ngx_queue_t* p, const ngx_queue_t* n);
+
+typedef struct yahoo_s {
+    ngx_queue_t   queue;
+} yahoo_t;
+
+typedef struct yahoo_guy_s {
+    ngx_uint_t    id;
+    u_char*       name;
+    ngx_queue_t   queue;
+} yahoo_guy_t;
+
